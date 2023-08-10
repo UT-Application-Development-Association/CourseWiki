537f8b4f0d26193c902a55c2b2fba5d08c783239
AceYang123 <leonisita@126.com>
2023-08-10 10:32:11 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="title">
+        Course Wiki
+      </div>
+      <div className="description">
+        Knowledge is the cheapest and most rewarding investment
+      </div>
+    </div>
+  );
+}
